562fcea0b61e7df6c924902646fc2a923e2e98b7
Feffe <feffe@chalmers.it>
2017-12-06 22:12:47 +0100

    Lab4 6.5

--- lab4/flipflop_irq_ack/startup.c
+++ lab4/flipflop_irq_ack/startup.c
@@ -13,7 +13,6 @@ asm volatile(
 }
 
 #define EXTI_PR 0x40013C14
-#define IRQ GPIO_E+0x14
 
 int count = 0;
 
@@ -22,34 +21,7 @@ void interrupt_handler(void)
 	// Kolla om avbrottet kommer från EXTI3
   if(*EXTI_PR & 4)
   {
-    //count++; // Öka count
-    //om avbrott IRQ0
-    if (*IRQ & 1) {
-      IRQ |= 0x10;
-      IRQ &= ~0x10;
-      count++;
-    }
-    //om avbrott IRQ1
-    if (*IRQ & 2) {
-
-      IRQ |= 0x20;
-      IRQ &= ~0x20;
-      count=0;
-    }
-    if (*IRQ & 4) {
-
-      IRQ |= 0x40;
-      IRQ &= ~0x40;
-      // om diodrampen är tänd
-      if (GPIO_D->odrHigh == 0xFF) {
-        GPIO_D->odrHigh = 0;
-      }
-      else{
-        GPIO_D->odrHight = 0xFF;
-      }
-
-
-    }
+    count++; // Öka count
     *((unsigned int *) 0x40013C14) |= 8; // Kvittera avbrott
   }
 }
@@ -57,7 +29,7 @@ void interrupt_handler(void)
 void app_init(void)
 {
   // Sätt upp PD0-7 som utport för visningsenhet
-    GPIO_D->moder = 0x55555555;
+    GPIO_D->moder = 0x00005555;
 
   // Koppla PE3 tillavbrottslina EXTI3
     #define SYSCFG_EXTICR1  0x40013808
@@ -73,8 +45,7 @@ void app_init(void)
     *((unsigned int *) 0x40013C0C) |= 8;
     // Maskera triggvillkor "positiv flank"
     *((unsigned int *) 0x40013C08) &= ~8;
-    // Open the gates
-    GPIO_E -> moder = 0x1500;
+
   // Sätt upp avbrottsvektor
   *((void (**)(void) ) 0x2001C064 ) = interrupt_handler;
 
