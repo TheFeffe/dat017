a55eb3da2d1633854c25168f29060b8a2947d405
Fredrik Åberg <abergf@net.chalmers.se>
2017-12-14 11:46:07 +0100

    labb5

--- lab5/pong/startup.c
+++ lab5/pong/startup.c
@@ -13,17 +13,21 @@ typedef enum {
     false = 0
 } bool;
 
+
+
 void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
 
 void startup ( void )
 {
-	asm volatile(
+	__asm volatile(
 		" LDR R0,=0x2001C000\n"		/* set stack */
 		" MOV SP,R0\n"
+		" LDR R0,=0x08000209\n BLX R0\n"
 		" BL main\n"				/* call main */
 		".L1: B .L1\n"				/* never return */
 	);
 }
+typedef char *String;
 
 #define TARGET_FPS 30
 
@@ -33,7 +37,7 @@ int winner = 0;
 void init_app(void){
 	//#ifdef USBDM
 		*((unsigned long *) 0x40023830) =0x18;
-		__asm volatile( " LDR R0,=0x08000209\n BLX R0\n");
+		//__asm volatile( " LDR R0,=0x08000209\n BLX R0\n");
 	//#endif
 
 	GPIO_D -> moder = 0x55005555;
@@ -60,7 +64,7 @@ typedef struct {
     void (*draw)(PBALL);
     void (*clear)(PBALL);
     void (*move)(PBALL);
-    void (*setDirection)(PBALL, int, int);
+    //void (*setDirection)(PBALL, int, int);
 } BALL, *PBALL;
 
 typedef struct {
@@ -71,80 +75,66 @@ typedef struct {
     void (*draw)(PPLAYER);
     void (*clear)(PPLAYER);
     void (*move)(PPLAYER);
-    void (*setDirection)(PPLAYER, int, int);
+    //void (*setDirection)(PPLAYER, int, int);
 } PLAYER, *PPLAYER;
 
 void drawPlayer(PPLAYER player) {
-    for (size_t i = 0; i < player->geo->length; i++) {
-        int x = player->xpos;
-        int y = player->ypos;
+    for (int i = 0; i < player->geo->length; i++) {
+        int x = player->xpos + player->geo->points[i].x;
+        int y = player->ypos + player->geo->points[i].y;
         pixel(x, y, 1);
     }
 }
 
 void clearPlayer(PPLAYER player) {
-    for (size_t i = 0; i < player->geo->length; i++) {
-        int x = player->xpos;
-        int y = player->ypos;
+    for (int i = 0; i < player->geo->length; i++) {
+        int x = player->xpos + player->geo->points[i].x;
+        int y = player->ypos + player->geo->points[i].y;
         pixel(x, y, 0);
     }
 }
 
 void movePlayer(PPLAYER player) {
     int ytmp = player->ypos + player->diry;
-    if(ytmp + player->geo->height >= 64||ytmp <= 0) {
+    if(ytmp + player->geo->height <= 64||ytmp >= 0) {
         player->ypos = ytmp;
     }
 }
 
 void drawBall(PBALL ball) {
-    for (size_t i = 0; i < ball->geo->length; i++) {
-        int x = ball->xpos;
-        int y = ball->ypos;
+    for (int i = 0; i < ball->geo->length; i++) {
+        int x = ball->xpos + ball->geo->points[i].x;
+        int y = ball->ypos + ball->geo->points[i].y;
         pixel(x, y, 1);
     }
 }
 
 void clearBall(PBALL ball) {
-    for (size_t i = 0; i < ball->geo->length; i++) {
-        int x = ball->xpos;
-        int y = ball->ypos;
+    for (int i = 0; i < ball->geo->length; i++) {
+        int x = ball->xpos + ball->geo->points[i].x;
+        int y = ball->ypos + ball->geo->points[i].y;
         pixel(x, y, 0);
     }
 }
 
-void moveBall(PBALL ball) {
+void moveBall(PBALL ball, PPLAYER p1, PPLAYER p2) {
     int xtmp = ball->xpos+ball->dirx;
     int ytmp = ball->ypos+ball->diry;
     // P1 längst till höger och P2 längst till vänster
-    if(collidesWithPlayer1(ball)||collidesWithPlayer2(ball)){
+    if(collidesWithPlayer1(ball, p1)||collidesWithPlayer2(ball, p2)){
         ball->dirx *= -1;
     }
-    if (xtmp < 0) {
+    if (xtmp < 0-1) {
         winner = 1;
     }
-    if (xtmp > 127) {
+    if (xtmp > 127+1) {
         winner = 2;
     }
     if(ytmp + ball->geo->height >= 64|| ytmp <= 0) {
         ball->diry *=-1;
     }
-}
-
-bool collidesWithPlayer1(PBALL ball) {
-    int xtmp = ball->xpos+ball->dirx;
-    int ytmp = ball->xpos+ball->diry;
-    return (xtmp + ball->geo->width >= p1.xpos && (ytmp >= p1->ypos && ytmp <= p1->ypos + p1->geo->height);
-}
-
-bool collidesWithPlayer2(PBALL ball) {
-    int xtmp = ball->xpos+ball->dirx;
-    int ytmp = ball->xpos+ball->diry;
-    return (xtmp <= p2.xpos+p2.geo->width && (ytmp >= p2->ypos && ytmp <= p2->ypos + p2->geo->height);
-}
-
-void setDirection(PBALL ball, int x, int y) {
-    /* code */
+	ball->xpos = ball->xpos+ball->dirx;
+	ball->ypos = ball->ypos+ball->diry;
 }
 
 static GEOMETRY ballGeometry = {
@@ -156,11 +146,11 @@ static GEOMETRY ballGeometry = {
         {1, 3}, {2, 3}, {3, 3}, {4, 3},
                 {2, 4}, {3, 4}
     }
-}
+};
 
 static GEOMETRY playerGeometry = {
-    16,
-    2, 8,
+    22,
+    2, 11,
     {
         {1, 1}, {2, 1},
         {1, 2}, {2, 2},
@@ -170,8 +160,12 @@ static GEOMETRY playerGeometry = {
         {1, 6}, {2, 6},
         {1, 7}, {2, 7},
         {1, 8}, {2, 8},
+		{1, 9}, {2, 9},
+        {1, 10}, {2, 10},
+        {1, 11}, {2, 11}
+		
     }
-}
+};
 
 static BALL leBall = {
     &ballGeometry,
@@ -179,9 +173,9 @@ static BALL leBall = {
     4, 2,
     drawBall,
     clearBall,
-    moveBall,
-    setDirectionBall
-}
+    moveBall//,
+    //setDirectionBall
+};
 
 static PLAYER p1 = {
     &playerGeometry,
@@ -190,9 +184,9 @@ static PLAYER p1 = {
     0,
     drawPlayer,
     clearPlayer,
-    movePlayer,
-    setDirectionPlayer
-}
+    movePlayer//,
+    //setDirectionPlayer
+};
 
 static PLAYER p2 = {
     &playerGeometry,
@@ -201,9 +195,28 @@ static PLAYER p2 = {
     0,
     drawPlayer,
     clearPlayer,
-    movePlayer,
-    setDirectionPlayer
+    movePlayer//,
+    //setDirectionPlayer
+};
+
+
+
+int collidesWithPlayer1(PBALL ball, PPLAYER p) {
+    int xtmp = ball->xpos+ball->dirx;
+    int ytmp = ball->ypos+ball->diry;
+	return xtmp >= p->xpos && (ytmp >= p->ypos - ball->geo->height && ytmp <= p->ypos + p->geo->height);
 }
+
+int collidesWithPlayer2(PBALL ball, PPLAYER p) {
+    int xtmp = ball->xpos+ball->dirx;
+    int ytmp = ball->ypos+ball->diry;
+	return xtmp <= p->xpos+p->geo->width && (ytmp >= p->ypos - ball->geo->height && ytmp <= p->ypos + p->geo->height);
+}
+
+void setDirection(PBALL ball, int x, int y) {
+    /* code */
+}
+
 
 int main(int argc, char **argv)
 {
@@ -211,10 +224,12 @@ int main(int argc, char **argv)
 	unsigned i;
  	init_app();
 	graphic_initialize();
-    ascii_init();
+	
 	graphic_clear_screen();
 
 	while(running){
+		p1.diry = 0;
+		p2.diry = 0;
 
         for(int i = 0; i < 40; i++){
             c=keyb();
@@ -223,37 +238,41 @@ int main(int argc, char **argv)
 
 		switch(c)
 		{
-			case 1: p2.diry = -4;
-			case 4: p2.diry = 4;
-			case 9: p1.diry = -4;
-			case 15: p1.diry = 4;
+			case 1: p2.diry = -4; break;
+			case 4: p2.diry = 4; break;
+			case 9: p1.diry = -4; break;
+			case 15: p1.diry = 4; break;
 		}
 
-        p1.clearPlayer(&p1);
-        p1.movePlayer(&p1);
-        p1.drawPlayer(&p1);
+        p1.clear(&p1);
+        p1.move(&p1);
+        p1.draw(&p1);
 
-        p2.clearPlayer(&p2);
-        p2.movePlayer(&p2);
-        p2.drawPlayer(&p2);
+        p2.clear(&p2);
+        p2.move(&p2);
+        p2.draw(&p2);
 
-        leBall.clearBall(&leBall);
-        leBall.moveBall(&leBall);
-        leBall.drawBall(&leBall);
+        leBall.clear(&leBall);
+        leBall.move(&leBall, &p1, &p2);
+        leBall.draw(&leBall);
 
         if(winner) break;
 	}
-
-    char winner_text[18]; // 18 is length of ascii row
-    char *s = winner_text;
-
+	
+	String winner_text;
+	char line1[] = "Player 1 Wins!";
+	char line2[] = "Player 2 Wins!";
+	
+	char * s = &winner_text;
     if(winner == 1)
     {
-        winner_text = "Player 1 Wins!";
+        //winner_text = "Player 1 Wins!";
+		s = line1;
     }
-    else if(winner == 2)
+    else
     {
-        winner_text = "Player 2 Wins!";
+        //winner_text = "Player 2 Wins!";
+		s = &line2;
     }
 
     ascii_gotoxy(1,1);
@@ -261,5 +280,9 @@ int main(int argc, char **argv)
     {
         ascii_write_char(*s++);
     }
+
+    //char winner_text[18]; // 18 is length of ascii row
+    //char *s = winner_text;
+
 
 }
